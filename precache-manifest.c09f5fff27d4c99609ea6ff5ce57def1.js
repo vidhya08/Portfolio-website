a8ae0c0d412c30ef8d990cfb090765e0bddced1f
vidhya08 <vidhyachaudhary08@gmail.com>
2020-06-03 15:30:06 +0530

    Updates

---
+++ precache-manifest.c09f5fff27d4c99609ea6ff5ce57def1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2495f0b6bb3c72162afc7f3b3a9834e1",
+    "url": "/Portfolio-website/index.html"
+  },
+  {
+    "revision": "9afb6110c05737140eac",
+    "url": "/Portfolio-website/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "8e44e981fa85f282d315",
+    "url": "/Portfolio-website/static/css/main.e3cf9413.chunk.css"
+  },
+  {
+    "revision": "9afb6110c05737140eac",
+    "url": "/Portfolio-website/static/js/2.3af622c9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Portfolio-website/static/js/2.3af622c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e44e981fa85f282d315",
+    "url": "/Portfolio-website/static/js/main.25713a4b.chunk.js"
+  },
+  {
+    "revision": "054f5e64025e305eac92",
+    "url": "/Portfolio-website/static/js/runtime-main.e60dbf44.js"
+  },
+  {
+    "revision": "f5779a203ef1425408c32c0141375646",
+    "url": "/Portfolio-website/static/media/about.f5779a20.jpg"
+  },
+  {
+    "revision": "5c9f8808fa8a238ee2a9e9b9a2591359",
+    "url": "/Portfolio-website/static/media/img_bg.5c9f8808.jpg"
+  }
+]);
